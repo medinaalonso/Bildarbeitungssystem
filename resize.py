7b34a318a953c018b98943f8316dc75f513c8cf9
mexicancoderalonso <48501743+mexicancoderalonso@users.noreply.github.com>
2019-09-10 01:47:02 -0500

    Add files via upload
    Cambiar tamaño de folder de imagenes

---
+++ resize.py
@@ -0,0 +1,10 @@
+import PIL
+from PIL import Image
+
+baseheight = 480
+for i in range(50):
+	img = Image.open(str(i)+".jpg")
+	hpercent = (baseheight / float(img.size[1]))
+	wsize = int((float(img.size[0]) * float(hpercent)))
+	img = img.resize((wsize, baseheight), PIL.Image.ANTIALIAS)
+	img.save(str(i)+".jpg")
